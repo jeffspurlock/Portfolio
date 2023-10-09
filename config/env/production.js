d209f734ef6aee1eead906b75d2ddb68c90fa8d6
Jeff Spurlock <jeffspurlock@icloud.com>
2023-10-09 12:19:46 -0500

    another change

--- config/env/production.js
+++ config/env/production.js
@@ -389,8 +389,8 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://example.com',
-    internalEmailAddress: 'support@example.com',
+    baseUrl: 'axonstudios.dev',
+    internalEmailAddress: 'jeff@axonstudios.dev',
 
     // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
     // stripeSecret: 'sk_prod__fake_Nfgh82401348jaDa3lkZ0d9Hm',
