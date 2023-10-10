e6d06340dca7cebe0a431feb858f596b05f84efb
Jeff Spurlock <jeffspurlock@icloud.com>
2023-10-10 16:50:28 -0500

    box animation refactored; need to fix name

--- assets/js/pages/homepage.js
+++ assets/js/pages/homepage.js
@@ -1,48 +1,61 @@
-function getObjectsDesktop(element, endCoords) {
+function getObjectsDesktop() {
     var navyEnd = { left: -30, top: 0 };
     var slateEnd = { left: 120, top: 40 };
     var mustardEnd = { left: 56, top: -75 };
-    var nameEnd = { left: 0, top: 0};
     var rectangleNavy = document.getElementById("rectangle-navy");
     var rectangleSlate = document.getElementById("rectangle-slate");
-    var rectangleMustard = document.getElementById("rectangle-mustard");
-    var name = document.getElementById("brand");
-    var collection = { 
-            navy : {
+    var rectangleMustard = document.getElementById("rectangle-mustard");  
+    var collection = [ 
+            {
                 element: rectangleNavy,
                 endCoord: navyEnd,
-             },
-            slate: {
+            },
+            {
                 element: rectangleSlate,
                 endCoord: slateEnd,
             },
-            mustard:
             {
                 element: rectangleMustard,
                 endCoord: mustardEnd,
             }
-        }
+    ];
     var package = [];
-    collection.foreach(rectangle => {
-        package.push(package(rectangle.element, rectangle.endCoord));
+    collection.forEach(rectangle => {
+        console.log(rectangle.element);
+        package.push(packageUp(rectangle.element, rectangle.endCoord));
     })
+
+    function packageUp(element, endCoords) {
+        console.log(element);
+        const left = $(element).css("left").substring(0, $(element).css("left").length - 2)*100/($(window).width());
+        const top = $(element).css("top").substring(0, $(element).css("top").length - 2)*100/($(window).width());
+        return { rectangle: element, left: left, top: top, leftEnd: endCoords.left, topEnd: endCoords.top }
+    }
+
     return package;
 };
-
-function package(element, endCoords) {
-    const left = $(element).css("left").substring(0, $(element).css("left").length - 2)*100/($(window).width());
-    const top = $(element).css("top").substring(0, $(element).css("top").length - 2)*100/($(window).width());
-    return { rectangle: element, left: left, top: top, leftEnd: endCoords.left, topEnd: endCoords.top }
-}
     
 
 async function animate(event, package){
+    var total = 200;
+    var current = window.scrollY;
+    var percent = (current / total);
+    if (current >= total){
+      percent = 1;
+    }
    
-   
-    package.foreach(item =>{
+    package.forEach(item =>{
      item.rectangle.style.left = (item.leftEnd - item.left) * percent + item.left + "vw";
      item.rectangle.style.top = (item.topEnd - item.top) * percent + item.top + "vw";
    })
+   var name = document.getElementById("brand");
+   var nameInit = {
+    left: $(name).css("left").substring(0, $(name).css("left").length - 2)*100/($(window).width()),
+    top: $(name).css("top").substring(0, $(name).css("top").length - 2)*100/($(window).width())
+   }
+   var nameEnd = { left: 0, top: 0};
+    name.style.left = (nameEnd.left - nameInit.left) * percent + nameInit.left + "vw";
+    name.style.top = (nameEnd.top - nameInit.top) * percent + nameInit.left + "vw";
   
     if(percent == 1){
       document.getElementById("gradient").style.display = "inline";
