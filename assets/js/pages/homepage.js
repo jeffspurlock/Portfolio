8ac19f4fdabb9468f5f0bbad85e6936bdcefccf4
Jeff Spurlock <jeffspurlock@icloud.com>
2023-10-11 11:12:08 -0500

    Refactored animate() to allow for mobile

--- assets/js/pages/homepage.js
+++ assets/js/pages/homepage.js
@@ -1,7 +1,15 @@
-function getObjectsDesktop() {
-    var navyEnd = { left: -30, top: 0 };
-    var slateEnd = { left: 120, top: 40 };
-    var mustardEnd = { left: 56, top: -75 };
+function getObjects(layout) {
+    if(layout == "desktop"){
+        var navyEnd = { left: -30, top: 0 };
+        var slateEnd = { left: 120, top: 40 };
+        var mustardEnd = { left: 56, top: -75 };
+        var brandEnd = { left: 0, top: 0, font: "3" };
+    } else if(layout == "mobile") {
+        var navyEnd = { left: -30, top: 0 };
+        var slateEnd = { left: 120, top: 40 };
+        var mustardEnd = { left: 56, top: -75 };
+        var brandEnd = { left: 0, top: 0, font: "3" };
+    };
     var rectangleNavy = document.getElementById("rectangle-navy");
     var rectangleSlate = document.getElementById("rectangle-slate");
     var rectangleMustard = document.getElementById("rectangle-mustard");  
@@ -19,11 +27,22 @@ function getObjectsDesktop() {
                 endCoord: mustardEnd,
             }
     ];
-    var package = [];
+    var rectPackage = [];
     collection.forEach(rectangle => {
         console.log(rectangle.element);
-        package.push(packageUp(rectangle.element, rectangle.endCoord));
+        rectPackage.push(packageUp(rectangle.element, rectangle.endCoord));
     })
+
+    var name = document.getElementById("brand");
+    var brand = {
+        element: name,
+        top: $(name).css("top").substring(0, $(name).css("top").length - 2)*100/($(window).width()),
+        left: $(name).css("left").substring(0, $(name).css("left").length - 2)*100/($(window).width()),
+        topEnd: brandEnd.top,
+        leftEnd: brandEnd.left,
+        font: $(name).css("font-size").substring(0, $(name).css("font-size").length - 2)*100/($(window).width()),
+        fontEnd: brandEnd.font
+    }
 
     function packageUp(element, endCoords) {
         console.log(element);
@@ -32,6 +51,8 @@ function getObjectsDesktop() {
         return { rectangle: element, left: left, top: top, leftEnd: endCoords.left, topEnd: endCoords.top }
     }
 
+    var package = { rectPackage: rectPackage, brand: brand };
+    console.log(package);
     return package;
 };
     
@@ -44,18 +65,14 @@ async function animate(event, package){
       percent = 1;
     }
    
-    package.forEach(item =>{
+    package.rectPackage.forEach(item =>{
      item.rectangle.style.left = (item.leftEnd - item.left) * percent + item.left + "vw";
      item.rectangle.style.top = (item.topEnd - item.top) * percent + item.top + "vw";
    })
-   var name = document.getElementById("brand");
-   var nameInit = {
-    left: $(name).css("left").substring(0, $(name).css("left").length - 2)*100/($(window).width()),
-    top: $(name).css("top").substring(0, $(name).css("top").length - 2)*100/($(window).width())
-   }
-   var nameEnd = { left: 0, top: 0};
-    name.style.left = (nameEnd.left - nameInit.left) * percent + nameInit.left + "vw";
-    name.style.top = (nameEnd.top - nameInit.top) * percent + nameInit.left + "vw";
+    
+    package.brand.element.style.left = (package.brand.leftEnd - package.brand.left) * percent + package.brand.left + "vw";
+    package.brand.element.style.top = (package.brand.topEnd - package.brand.top) * percent + package.brand.top + "vw";
+    package.brand.element.style.fontSize = (package.brand.fontEnd - package.brand.font) * percent + package.brand.font + "vw";
   
     if(percent == 1){
       document.getElementById("gradient").style.display = "inline";
